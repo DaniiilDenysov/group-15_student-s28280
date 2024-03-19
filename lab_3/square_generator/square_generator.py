7135e439216df62ac8d9608dfa4092e9ba4a5484
s28280 <s28280@pjwstk.edu.pl>
2024-03-19 10:18:06 +0100

    Completed task 10: converted SquareGenerator into abstract class and made generate_squares function abstract method

--- lab_3/square_generator/square_generator.py
+++ lab_3/square_generator/square_generator.py
@@ -1,7 +1,7 @@
-class SquareGenerator:
-    @staticmethod
-    def generate_squares(a, b):
-        if a > b:
-            raise ValueError("The value of 'a' cannot be greater than 'b'")
-        squares = [x * x for x in range(a, b)]
-        return squares
+from abc import ABC, abstractmethod
+
+
+class SquareGenerator(ABC):
+    @abstractmethod
+    def generate_squares(self, a, b):
+        pass
