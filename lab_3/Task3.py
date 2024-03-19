bcf264318956e1d5a28e9ef6aa5067b1cace8ebb
s28280 <s28280@pjwstk.edu.pl>
2024-03-19 09:52:38 +0100

    Completed task 5: modified Task3 file, added check for valid numbers and exception in case of wrong numbers

--- lab_3/Task3.py
+++ lab_3/Task3.py
@@ -1,5 +1,7 @@
 class SquareGenerator:
     @staticmethod
     def generate_squares(a, b):
+        if a > b:
+            raise ValueError("The value of 'a' cannot be greater than 'b'")
         squares = [x * x for x in range(a, b)]
         return squares
